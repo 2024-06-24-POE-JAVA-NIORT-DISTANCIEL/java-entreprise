042caefd71ed93b1d5f4803eae271261e218c1dc
François Caremoli <francois.caremoli@wanadoo.fr>
2024-07-15 22:47:19 +0200

    Premier commit

---
+++ src/test/java/com/bigcorp/booking/service/MonPremierTest.java
@@ -0,0 +1,12 @@
+package com.bigcorp.booking.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MonPremierTest  {
+
+    @Test
+    public void testInutile() throws Exception {
+       Assertions.assertTrue(true);
+    }
+}
